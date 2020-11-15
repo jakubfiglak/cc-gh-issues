6020a04da496a5552f48c44b665a8166793e7e8a
Jakub Figlak <jakub.figlak@gmail.com>
2020-11-15 19:37:24 +0100

    Prepare the app for deployment

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import githubRoutes from './routes/github';
 import { errorHandler } from './middleware/error';
 
@@ -10,6 +11,14 @@ const app = express();
 app.use(express.json());
 
 app.use('/github', githubRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 app.use(errorHandler);
 
